9daa8d32b5c4ba2d8ff992bbfe71b56cd729df1c
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-03-16 14:53:25 +0900

    Add upload image

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module BoardApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.generators do |g|
       g.skip_routes true
       g.helper false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_063616) do
+ActiveRecord::Schema.define(version: 2021_03_16_050825) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_063616) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["first_name"], name: "index_users_on_first_name"
     t.index ["last_name"], name: "index_users_on_last_name"
